d0f9325745511299fad9f5457d0fa3e460991a39
rotem.bu <rotem.bu@gmail.com>
2017-03-19 09:25:58 -0700

    first chek in

---
+++ backbank/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backbank
+{
+    class Account
+    {
+        #region Properties
+        /// <summary>
+        /// Account number of the account
+        /// </summary>
+        public int AccountNumber { get;  }
+        /// <summary>
+        /// Email address of the account
+        /// </summary>
+        public string EmailAddress { get; set; }
+
+        public decimal Balance { get; private set; }
+
+        public string TypeOfAcconut { get; set; }
+
+        #endregion
+        #region methods
+        public void deposit(decimal amount)
+        {
+            // Balance= Balance+amount
+            Balance += amount;
+        }
+             public void Withdraw(decimal amount)
+        { Balance -= amount; 
+      
+    }
+   
+
+    
+}
---
+++ backbank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backbank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var account1 = new Account();
+           account1.EmailAddress="test@test.com"
+        account1.TypeOfAcconut = "checking";
+            account1.deposit(105.40M);
+            Console.WriteLine($"Accountnumber: {account1.}")
+
+        }
+    }
+}
